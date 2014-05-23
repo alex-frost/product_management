4bb2f17714bba25842020b66377298278eb1f5e6
Alex Frost <alex.frost.dev@gmail.com>
2014-05-23 15:29:39 +0100

    rm unused routes

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -7,16 +7,8 @@ describe ProductsController do
       get("/products").should route_to("products#index")
     end
 
-    it "routes to #new" do
-      get("/products/new").should route_to("products#new")
-    end
-
     it "routes to #show" do
       get("/products/1").should route_to("products#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/products/1/edit").should route_to("products#edit", :id => "1")
     end
 
     it "routes to #create" do
