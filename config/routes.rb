35ba8febcd4455c572400b16e04e74a038e19a1f
Alex Frost <alex.frost.dev@gmail.com>
2014-05-23 19:06:47 +0100

    rails g scaffold LineItem product:references order:references quantity:integer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ProductManagement::Application.routes.draw do
+  resources :line_items, except: [:new, :edit]
   resources :orders, except: [:new, :edit]
   resources :products, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523145733) do
+ActiveRecord::Schema.define(version: 20140523180612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
   create_table "orders", force: true do |t|
     t.date     "date"
