6e7ac585913811d5307e4fb750fd1ba101c16b60
Alex Frost <alex.frost.dev@gmail.com>
2014-05-23 19:25:35 +0100

    rm unused routes

--- spec/routing/line_items_routing_spec.rb
+++ spec/routing/line_items_routing_spec.rb
@@ -7,16 +7,8 @@ describe LineItemsController do
       get("/line_items").should route_to("line_items#index")
     end
 
-    it "routes to #new" do
-      get("/line_items/new").should route_to("line_items#new")
-    end
-
     it "routes to #show" do
       get("/line_items/1").should route_to("line_items#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/line_items/1/edit").should route_to("line_items#edit", :id => "1")
     end
 
     it "routes to #create" do
