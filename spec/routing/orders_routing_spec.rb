0dfb0e37a2427845ce6c7709d272edcd5535ae61
Alex Frost <alex.frost.dev@gmail.com>
2014-05-23 16:09:20 +0100

    rm unused routes

--- spec/routing/orders_routing_spec.rb
+++ spec/routing/orders_routing_spec.rb
@@ -7,16 +7,8 @@ describe OrdersController do
       get("/orders").should route_to("orders#index")
     end
 
-    it "routes to #new" do
-      get("/orders/new").should route_to("orders#new")
-    end
-
     it "routes to #show" do
       get("/orders/1").should route_to("orders#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/orders/1/edit").should route_to("orders#edit", :id => "1")
     end
 
     it "routes to #create" do
