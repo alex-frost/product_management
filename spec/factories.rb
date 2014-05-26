9ed0b0735bd7222b6f82875aa8dce8d84a191b3b
Alex Frost <alex.frost.dev@gmail.com>
2014-05-26 16:02:42 +0100

    add factories

---
+++ spec/factories.rb
@@ -0,0 +1,25 @@
+FactoryGirl.define do
+  sequence(:product_name) { |n| "My product nos. #{n}" }
+
+  factory :product do
+    name { generate(:product_name) }
+    net_price 1.2
+  end
+
+  factory :order do
+    date Date.current + 2.days
+  end
+
+  factory :line_item do
+    quantity 1
+    product {
+      FactoryGirl.create(:product)
+    }
+  end
+
+  trait :with_line_items do
+    after :create do |order|
+      FactoryGirl.create_list :line_item, 2, order: order
+    end
+  end
+end
