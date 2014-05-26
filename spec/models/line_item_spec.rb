8fb63ec34c7e13c11d74d19305a652e1aade6e3b
Alex Frost <alex.frost.dev@gmail.com>
2014-05-26 08:34:24 +0100

    add spec for generated relations

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe LineItem do
   it { should validate_numericality_of(:quantity).is_greater_than(0) }
+
+  it { should belong_to(:product) }
+  it { should belong_to(:order) }
 end
