d34d8b254725361a8adcef146b29232175421c4a
Alex Frost <alex.frost.dev@gmail.com>
2014-05-26 08:34:03 +0100

    add product relations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :net_price, numericality: {greater_than_or_equal_to: 0}
+
+  has_many :line_items
+  has_many :orders, through: :line_items
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
   it { should validate_numericality_of(:net_price).is_greater_than_or_equal_to(0) }
+
+  it { should have_many(:line_items) }
+  it { should have_many(:orders).through(:line_items) }
+
 end
