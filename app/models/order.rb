a44c499d5da9ad42300baab15c2a770f7ed93429
Alex Frost <alex.frost.dev@gmail.com>
2014-05-26 08:33:47 +0100

    add order relations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ActiveRecord::Base
   validates :status, inclusion: { in: %w(DRAFT PLACED PAID CANCELLED),
                                   message: "%{value} is not a valid status. Must be DRAFT, PLACED, PAID, or CANCELLED" }
+
+  has_many :line_items, inverse_of: :order
+  has_many :products, through: :line_items
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,4 +5,7 @@ describe Order do
        in_array(%w(DRAFT PLACED PAID CANCELLED)).
        with_message("%{value} is not a valid status. Must be DRAFT, PLACED, PAID, or CANCELLED")
   }
+
+  it { should have_many(:line_items) }
+  it { should have_many(:products).through(:line_items) }
 end
