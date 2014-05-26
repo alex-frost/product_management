69fb0f1491d978ff75b6d7608a39244ffc08b6f0
Alex Frost <alex.frost.dev@gmail.com>
2014-05-26 15:36:50 +0100

    check if order is DRAFT before updating

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,12 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
 
   validates :quantity, numericality: {greater_than: 0}
+
+  validate :order_status_is_draft
+
+  def order_status_is_draft
+    unless order.status == 'DRAFT'
+      errors.add(:order_id, "Order status is not DRAFT")
+    end
+  end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,8 +1,33 @@
 require 'spec_helper'
 
 describe LineItem do
+  let(:order) { Order.create! ({ "date" => "2014-05-23"}) }
+  subject {order.line_items.new}
+
   it { should validate_numericality_of(:quantity).is_greater_than(0) }
 
   it { should belong_to(:product) }
   it { should belong_to(:order) }
+
+  describe 'validates order_status_is_draft' do
+    let(:product) { Product.create! ({ "name" => "New Product", "net_price" => 1.23 }) }
+
+    subject! {order.line_items.create! ({"product_id" => product.to_param, "quantity" => 3}) }
+
+    context "when order is DRAFT" do
+      it "can be updated" do
+        expect(subject.update ({quantity: 4})).to be_true
+      end
+    end
+
+    context "when order is PLACED" do
+      before :each do
+        order.update(status: 'PLACED')
+      end
+
+      it 'cannot be updated' do
+        expect(subject.update ({quantity: 4})).to be_false
+      end
+    end
+  end
 end
